e5fbbe3d4ace10b6217b22a12875f9cd98d9a12f
lior <lior610.aharon@gmail.com>
2023-05-12 02:08:56 +0300

    added independent display book site

---
+++ book_display/static/scripts/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function() {
+    adjustBookImageSize();
+});
+
+window.addEventListener('resize', function() {
+  adjustBookImageSize();
+});
+
+function adjustBookImageSize() {
+  var bookImage = document.querySelector('.book-image');
+  var viewportWidth = window.innerWidth;
+  
+  if (viewportWidth < 768) {
+    bookImage.style.maxWidth = '200px';
+    bookImage.style.maxHeight = '300px';
+  } else {
+    bookImage.style.maxWidth = '300px';
+    bookImage.style.maxHeight = '400px';
+  }
+}
